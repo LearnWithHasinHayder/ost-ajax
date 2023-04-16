bc4b3040e8eebd8359ed7fdb31fa8081f714b74e
Hasin Hayder <countdraculla@gmail.com>
2023-04-16 23:51:55 +0600

    Update

--- ajax.js
+++ ajax.js
@@ -1,5 +1,5 @@
 function simpleGet() {
-    fetch('http://127.0.0.1:8000/api/ajax/get')
+    fetch('http://127.0.0.1:8000/api/ajax/get/Ostad')
         .then(r => r.text())
         .then(data => {
             console.log(data)
@@ -11,14 +11,14 @@ function getJSON() {
         .then(r => r.json())
         .then(data => {
             console.log(data)
-            console.log(data.email)
+            console.log(data.name)
         })
 }
 
 function sendHeader() {
     fetch('http://127.0.0.1:8000/api/ajax/getHeader', {
         headers: {
-            CustomHeader: '123'
+            CustomHeader: '123456'
         }
     })
         .then(r => r.json())
@@ -30,6 +30,7 @@ function sendHeader() {
 function errorCheck() {
     fetch('http://127.0.0.1:8000/api/ajax/getData/2')
         .then(r => {
+            console.log(r.ok, r.status);
             return {
                 json: r.json(),
                 ok: r.ok,
@@ -44,7 +45,7 @@ function errorCheck() {
 function postRequest() {
     fetch('http://127.0.0.1:8000/api/ajax/post', {
         method: 'POST',
-        body: JSON.stringify({ name: 'John Doe' }),
+        body: JSON.stringify({ name: 'Jane Doe' }),
         headers: {
             "Content-Type": "application/json"
         }
@@ -58,6 +59,20 @@ function postRequest() {
 function putRequest() {
     fetch('http://127.0.0.1:8000/api/ajax/put/1', {
         method: 'PUT',
+        body: JSON.stringify({ name: 'John Doe' }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
+function patchRequest() {
+    fetch('http://127.0.0.1:8000/api/ajax/patch/1', {
+        method: 'PATCH',
         body: JSON.stringify({ name: 'John Doe' }),
         headers: {
             "Content-Type": "application/json"
@@ -95,6 +110,7 @@ function getJSONAxios() {
     axios.get("http://127.0.0.1:8000/api/ajax/getJSON")
         .then(data => {
             console.log(data)
+            console.log(data.data.email)
         })
 }
 
@@ -115,46 +131,47 @@ function sendHeaderAxios() {
         }
     })
         .then(data => {
-            console.log(data)
+            console.log(data.data)
         })
 }
 
 function postRequestAxios() {
-    axios.post('http://127.0.0.1:8000/api/ajax/post', { name: 'John Doe' })
+    axios.post('http://127.0.0.1:8000/api/ajax/post', { name: 'Jimmy Doe' })
         .then(data => {
-            console.log(data)
+            console.log(data.data)
         })
 }
 
 function putRequestAxios() {
     axios.put('http://127.0.0.1:8000/api/ajax/put/1', { name: 'John Doe' })
         .then(data => {
-            console.log(data)
+            console.log(data.data)
         })
 }
 
 function patchRequestAxios() {
     axios.patch('http://127.0.0.1:8000/api/ajax/patch/1', { name: 'John Doe' })
         .then(data => {
-            console.log(data)
+            console.log(data.data)
         })
 }
 
 function deleteRequestAxios() {
     axios.delete('http://127.0.0.1:8000/api/ajax/delete/1')
         .then(data => {
-            console.log(data)
+            console.log(data.data)
         })
 }
 //------------------
-document.getElementById('submit-button').addEventListener('click', 
-function handleForm(event) {
-    event.preventDefault()
-    const form = document.getElementById('form')
-    const formdata = Object.fromEntries(new FormData(form))
-    axios.post('http://127.0.0.1:8000/api/ajax/post', formdata)
-        .then(data => {
-            console.log(data)
-        })
+document.getElementById('submit-button').addEventListener('click',
+    function handleForm(event) {
+        event.preventDefault()
+        const form = document.getElementById('form')
+        const formdata = Object.fromEntries(new FormData(form))
+        console.log(formdata)
+        axios.post('http://127.0.0.1:8000/api/ajax/post', formdata)
+            .then(data => {
+                console.log(data.data)
+            })
 
-})
+    })
